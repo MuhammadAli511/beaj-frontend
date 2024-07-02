58b9b0d4b431204842e51b36268ed8158a355adf
Muhammad Ali <mohammadali417@outlook.com>
2024-07-03 04:03:43 +0500

    Model Text

--- src/pages/ChatbotLogs/ChatbotLogs.jsx
+++ src/pages/ChatbotLogs/ChatbotLogs.jsx
@@ -77,19 +77,25 @@ const ChatbotLogs = () => {
                     <thead className={styles.heading_row}>
                         <tr>
                             <th className={styles.table_heading}>ID</th>
-                            <th className={styles.table_heading}>User Audio</th>
-                            <th className={styles.table_heading}>Model Audio</th>
                             <th className={styles.table_heading}>User Speech to Text Time (sec)</th>
                             <th className={styles.table_heading}>Model Feedback Time (sec)</th>
                             <th className={styles.table_heading}>Model Text to Speech Time (sec)</th>
                             <th className={styles.table_heading}>Total Time (sec)</th>
                             <th className={styles.table_heading}>Prompt</th>
+                            <th className={styles.table_heading}>User Audio</th>
+                            <th className={styles.table_heading}>Model Audio</th>
+                            <th className={styles.table_heading}>Model Text</th>
                         </tr>
                     </thead>
                     <tbody className={styles.table_body}>
                         {chatbotLogs.map(log => (
                             <tr key={log.id} className={styles.table_row}>
                                 <td className={styles.table_cell}>{log.id}</td>
+                                <td className={styles.table_cell}>{roundToTwo(log.userSpeechToTextTime)}</td>
+                                <td className={styles.table_cell}>{roundToTwo(log.modelFeedbackTime)}</td>
+                                <td className={styles.table_cell}>{roundToTwo(log.modelTextToSpeechTime)}</td>
+                                <td className={styles.table_cell}>{roundToTwo(log.totalTime)}</td>
+                                <td className={styles.table_cell_text}>{log.modelPrompt}</td>
                                 <td className={styles.table_cell}>
                                     <audio controls>
                                         <source src={log.userAudio} type="audio/ogg" />
@@ -102,11 +108,7 @@ const ChatbotLogs = () => {
                                         Your browser does not support the audio element.
                                     </audio>
                                 </td>
-                                <td className={styles.table_cell}>{roundToTwo(log.userSpeechToTextTime)}</td>
-                                <td className={styles.table_cell}>{roundToTwo(log.modelFeedbackTime)}</td>
-                                <td className={styles.table_cell}>{roundToTwo(log.modelTextToSpeechTime)}</td>
-                                <td className={styles.table_cell}>{roundToTwo(log.totalTime)}</td>
-                                <td className={styles.table_cell}>{log.modelPrompt}</td>
+                                <td className={styles.table_cell_text}>{log.modelText}</td>
                             </tr>
                         ))}
                     </tbody>
