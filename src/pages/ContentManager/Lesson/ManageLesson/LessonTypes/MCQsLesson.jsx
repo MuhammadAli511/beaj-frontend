ad14c3a0cafa042927b2a17a61ca2642174d9a70
Muhammad Ali <mohammadali417@outlook.com>
2024-08-13 16:20:48 +0500

    mcq lesson edit, delete

--- src/pages/ContentManager/Lesson/ManageLesson/LessonTypes/MCQsLesson.jsx
+++ src/pages/ContentManager/Lesson/ManageLesson/LessonTypes/MCQsLesson.jsx
@@ -1,47 +1,303 @@
-import React, { useState, useEffect } from 'react';
-import { getLessonsByActivity } from '../../../../../helper';
-import edit from '../../../../../assets/images/edit.svg';
-import deleteIcon from '../../../../../assets/images/delete.svg';
-import styles from './MCQsLesson.module.css';
-import MCQsQuestionModal from './MCQsQuestionModal';
+import React, { useState, useEffect } from "react";
+import {
+    getLessonsByActivity,
+    getLessonById,
+    deleteLesson,
+    getAllCourses,
+    getAllActivityAliases,
+    updateLesson,
+    updateMultipleChoiceQuestion,
+    deleteMultipleChoiceQuestion,
+    updateMultipleChoiceQuestionAnswer,
+    deleteMultipleChoiceQuestionAnswer,
+} from "../../../../../helper";
+import edit from "../../../../../assets/images/edit.svg";
+import deleteIcon from "../../../../../assets/images/delete.svg";
+import styles from "./MCQsLesson.module.css";
+import MCQsQuestionModal from "./MCQsQuestionModal";
 
-const MCQsLesson = ({ category, course, activity }) => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [lessons, setLessons] = useState([]);
-    const [selectedLesson, setSelectedLesson] = useState(null);
+const EditMCQLessonModal = ({ isOpen, onClose, lesson, onSave, activity }) => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [isSaving, setIsSaving] = useState(false);
+    const [lessonData, setLessonData] = useState(null);
+    const [questions, setQuestions] = useState([]);
+    const [courses, setCourses] = useState([]);
+    const [activityAliases, setActivityAliases] = useState([]);
 
     useEffect(() => {
-        const fetchLessons = async () => {
+        if (lesson) {
             try {
                 setIsLoading(true);
-                const lessonsResponse = await getLessonsByActivity(course, activity);
-                if (lessonsResponse.status === 200) {
-                    setLessons(lessonsResponse.data);
-                } else {
-                    alert(lessonsResponse.data.message);
-                }
+                fetchAllData();
             } catch (error) {
                 alert(error);
             } finally {
                 setIsLoading(false);
             }
-        };
+        }
+    }, [lesson]);
+
+    const fetchLessonData = async () => {
+        try {
+            const lessonResponse = await getLessonById(lesson.LessonId);
+            if (lessonResponse.status === 200) {
+                const fetchedQuestions = lessonResponse.data.multipleChoiceQuestions || [];
+                setLessonData(lessonResponse.data);
+                setQuestions(fetchedQuestions.sort((a, b) => a.questionNumber - b.questionNumber));
+            } else {
+                alert(lessonResponse.data.message);
+            }
+        } catch (error) {
+            alert(error);
+        }
+    };
+
+    const fetchCourses = async () => {
+        try {
+            const response = await getAllCourses();
+            if (response.status === 200) {
+                setCourses(response.data);
+            } else {
+                alert(response.data.message);
+            }
+        } catch (error) {
+            alert(error);
+        }
+    };
+
+    const fetchActivityAliases = async () => {
+        try {
+            const response = await getAllActivityAliases();
+            if (response.status === 200) {
+                setActivityAliases(response.data);
+            } else {
+                alert(response.data.message);
+            }
+        } catch (error) {
+            alert(error);
+        }
+    };
+
+    const fetchAllData = async () => {
+        const promises = [
+            fetchCourses(),
+            fetchLessonData(),
+            fetchActivityAliases(),
+        ];
+        await Promise.all(promises);
+    };
+
+    const handleCancel = () => {
+        setLessonData(null);
+        setCourses([]);
+        setActivityAliases([]);
+        onClose();
+    };
+
+    const sortedCourses = () => {
+        if (!lessonData) return courses;
+        const sorted = [...courses].sort((a, b) =>
+            a.CourseId === lessonData.courseId
+                ? -1
+                : b.CourseId === lessonData.courseId
+                    ? 1
+                    : 0
+        );
+        return sorted;
+    };
+
+    const sortedActivityAliases = () => {
+        if (!lessonData) return activityAliases;
+        const sorted = [...activityAliases].sort((a, b) =>
+            a.Alias === lessonData.activityAlias
+                ? -1
+                : b.Alias === lessonData.activityAlias
+                    ? 1
+                    : 0
+        );
+        return sorted;
+    };
+
+    return (
+        <div>
+            {isOpen && (
+                <div className={styles.modal}>
+                    <div className={styles.modalContent}>
+                        <h2 className={styles.modal_heading}>Edit MCQ Lesson</h2>
+                        {isLoading && <div>Loading...</div>}
+                        {!isLoading && lessonData && (
+                            <div>
+                                <div className={styles.form_group}>
+                                    <label className={styles.label} htmlFor="course_id">Select Course</label>
+                                    <select
+                                        className={styles.input_field}
+                                        id="course_id"
+                                        defaultValue={lessonData.courseId || "Not Available"}
+                                    >
+                                        {sortedCourses().map((course) => (
+                                            <option key={course.CourseId} value={course.CourseId}>
+                                                {course.CourseName}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+                                <div className={styles.form_group}>
+                                    <label className={styles.label}>Sequence Number</label>
+                                    <input
+                                        className={styles.input_field}
+                                        type="text"
+                                        id="sequenceNumber"
+                                        defaultValue={lessonData.SequenceNumber || "Not Available"}
+                                    />
+                                </div>
+                                <div className={styles.form_group}>
+                                    <label className={styles.label}>Week Number</label>
+                                    <input
+                                        className={styles.input_field}
+                                        type="text"
+                                        id="weekNumber"
+                                        defaultValue={lessonData.weekNumber || "Not Available"}
+                                    />
+                                </div>
+                                <div className={styles.form_group}>
+                                    <label className={styles.label}>Day Number</label>
+                                    <input
+                                        className={styles.input_field}
+                                        type="text"
+                                        id="dayNumber"
+                                        defaultValue={lessonData.dayNumber || "Not Available"}
+                                    />
+                                </div>
+                                <div className={styles.form_group}>
+                                    <label className={styles.label} htmlFor="activity_alias">Select Activity Alias</label>
+                                    <select
+                                        className={styles.input_field}
+                                        id="activity_alias"
+                                        defaultValue={lessonData.activityAlias || "Not Available"}
+                                    >
+                                        {sortedActivityAliases().map((activityAlias) => (
+                                            <option key={activityAlias.id} value={activityAlias.Alias}>
+                                                {activityAlias.Alias}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+                                <div className={styles.form_group}>
+                                    <label className={styles.label}>Status</label>
+                                    <select
+                                        className={styles.input_field}
+                                        id="status"
+                                        defaultValue={lessonData.status || "Not Available"}
+                                    >
+                                        <option value="Active">Active</option>
+                                        <option value="Not Active">Not Active</option>
+                                    </select>
+                                </div>
+
+
+                                <div className={styles.form_group_row}>
+                                    <button
+                                        className={styles.submit_button}
+                                        // onClick={handleSave}
+                                        disabled={isSaving}
+                                    >
+                                        {isSaving ? <div className="loader"></div> : "Save Changes"}
+                                    </button>
+                                    <button
+                                        className={styles.cancel_button}
+                                        onClick={handleCancel}
+                                        disabled={isSaving}
+                                    >
+                                        Cancel
+                                    </button>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+
+const MCQsLesson = ({ category, course, activity }) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [lessons, setLessons] = useState([]);
+    const [selectedLesson, setSelectedLesson] = useState(null);
+    const [isMCQModalOpen, setIsMCQModalOpen] = useState(false);
+    const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+
+    const fetchLessons = async () => {
+        try {
+            setIsLoading(true);
+            const lessonsResponse = await getLessonsByActivity(course, activity);
+            if (lessonsResponse.status === 200) {
+                setLessons(lessonsResponse.data);
+            } else {
+                alert(lessonsResponse.data.message);
+            }
+        } catch (error) {
+            alert(error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
         if (category !== "" && course !== "") {
             fetchLessons();
         }
     }, [category, course, activity]);
 
-    const openModal = (lesson) => {
+    const openMCQModal = (lesson) => {
         setSelectedLesson(lesson);
+        setIsMCQModalOpen(true);
     };
 
-    const closeModal = () => {
+    const closeMCQModal = () => {
         setSelectedLesson(null);
+        setIsMCQModalOpen(false);
     };
+
+    const openEditModal = (lesson) => {
+        setSelectedLesson(lesson);
+        setIsEditModalOpen(true);
+    };
+
+    const closeEditModal = () => {
+        setSelectedLesson(null);
+        setIsEditModalOpen(false);
+    };
+
+    const handleDeleteLesson = async (lessonId) => {
+        const isConfirmed = window.confirm("Are you sure you want to delete this lesson?");
+        if (isConfirmed) {
+            try {
+                const deleteResponse = await deleteLesson(lessonId);
+                if (deleteResponse.status === 200) {
+                    alert("Lesson deleted successfully");
+                    fetchLessons();
+                } else {
+                    alert(deleteResponse.data.message);
+                }
+            } catch (error) {
+                alert(error);
+            }
+        }
+    };
+
+    const activityMapper = {
+        'mcqs': 'Multiple Choice Questions',
+        'postMCQs': 'Post Multiple Choice Questions',
+        'preMCQs': 'Pre Multiple Choice Questions',
+        'placementTest': 'Placement Test',
+    };
+
 
     return (
         <div>
-            <h1 className={styles.heading}>Manage your MCQs lessons</h1>
+            <h1 className={styles.heading}>Manage your {activityMapper[activity]} lessons</h1>
             {/* loading */}
             {isLoading && <div>Fetching Data...</div>}
             {/* no data */}
@@ -68,18 +324,47 @@ const MCQsLesson = ({ category, course, activity }) => {
                                 <td style={{ width: "15%" }}>{lesson.weekNumber}</td>
                                 <td style={{ width: "15%" }}>{lesson.dayNumber}</td>
                                 <td style={{ width: "15%" }}>
-                                    <button className={styles.submit_button} onClick={() => openModal(lesson)}>Show Questions</button>
+                                    <button
+                                        className={styles.submit_button}
+                                        onClick={() => openMCQModal(lesson)}
+                                    >
+                                        Show Questions
+                                    </button>
                                 </td>
-                                <td style={{ width: "10%" }}><img src={edit} alt="Edit" /></td>
-                                <td style={{ width: "10%" }}><img src={deleteIcon} alt="Delete" /></td>
+                                <td style={{ width: "10%" }}>
+                                    <img
+                                        onClick={() => openEditModal(lesson)}
+                                        src={edit}
+                                        alt="Edit"
+                                    />
+                                </td>
+                                <td style={{ width: "10%" }}>
+                                    <img
+                                        onClick={() => handleDeleteLesson(lesson.LessonId)}
+                                        src={deleteIcon}
+                                        alt="Delete"
+                                    />
+                                </td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
             )}
-            {selectedLesson && (
-                console.log(selectedLesson),
-                <MCQsQuestionModal lesson={selectedLesson} onClose={closeModal} activity={activity} />
+            {isMCQModalOpen && selectedLesson && (
+                <MCQsQuestionModal
+                    lesson={selectedLesson}
+                    onClose={closeMCQModal}
+                    activity={activity}
+                />
+            )}
+            {isEditModalOpen && selectedLesson && (
+                <EditMCQLessonModal
+                    isOpen={isEditModalOpen}
+                    onClose={closeEditModal}
+                    lesson={selectedLesson}
+                    onSave={fetchLessons}
+                    activity={activity}
+                />
             )}
         </div>
     );
